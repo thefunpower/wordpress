ac320a504cc96eb985f2b470efe8d1e05834f70b
sunkang <13127955213@qq.com>
2023-10-11 14:06:34 +0800

    v1.0.0

--- boot.php
+++ boot.php
@@ -25,21 +25,23 @@ if(!function_exists('g_init_wordpress')){
 			include __DIR__.'/../db_medoo/inc/db/boot.php'; 
 			include __DIR__.'/../core/helper.php';  
 			add_action( 'init', "express_install");   
-			include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); 
-
-		    function g($key = ''){
-			   	if($key){
-			   		return $_POST[$key];
-			   	}
-			   	return $_POST;
-			}
-
-		    function guzzle_http($click_option = [])
-			{
-			    $click_option['timeout'] = $click_option['timeout'] ?: 60;
-			    $client = new \GuzzleHttp\Client($click_option);
-			    return $client;
-			}
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );  
 		} 
 	}
+
+
+	function g($key = ''){
+	   	if($key){
+	   		return $_POST[$key];
+	   	}
+	   	return $_POST;
+	}
+
+    function guzzle_http($click_option = [])
+	{
+	    $click_option['timeout'] = $click_option['timeout'] ?: 60;
+	    $client = new \GuzzleHttp\Client($click_option);
+	    return $client;
+	}
+	
 }
