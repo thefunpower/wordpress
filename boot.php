5d53ee033c18740a57cf7e51a33c39a2f8bf9c4b
sunkang <13127955213@qq.com>
2023-10-26 13:38:38 +0800

    Update boot.php

--- boot.php
+++ boot.php
@@ -18,33 +18,4 @@ function g_init_wordpress(){
 		include __DIR__.'/../db_medoo/inc/db/boot.php';  
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );  
 	} 
-}  
-
- 
-function get_req_post($key = "")
-{
-    $input = get_input();
-    $data  = $_POST;
-    if ($key) {
-        $val = $data[$key];
-        if ($input && is_array($input) && !$val) {
-            $val = $input[$key];
-        }
-        return $val;
-    } else {
-        return $data ?: $input;
-    }
-} 
-
-function g($key = null)
-{
-    $val = get_req_post($key);
-    if (!$val) {
-        $val = $key?$_GET[$key]:$_GET;
-    }
-    global $config;
-    if($config['xss_clean'] == true){
-        $val = xss_clean($val);
-    }
-    return $val;
-} 
+}
